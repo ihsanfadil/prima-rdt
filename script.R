5c8388aeb1d0e64174b253403e62d9c031194e7d
ihsanfadil <ifadilah@oucru.org>
2024-02-21 21:55:40 +0700

    initial code.

--- script.R
+++ script.R
@@ -9,18 +9,18 @@
 # Setup -------------------------------------------------------------------
 
 library(tidyverse)
-library(readxl)
 library(ggbeeswarm)
 library(broom)
 library(scales)
-library(here)
 library(ggExtra)
+library(ggeffects)
 library(xtable); library(knitr)
 library(extrafont); loadfonts()
 
+options(scipen = 999)
 theme_set(theme_bw())
 theme_update(
-  text = element_text(size = 9.5, family = "Fira Code"), # Font
+  text = element_text(size = 8.5, family = "Fira Code"), # Font
   plot.title = element_text(hjust = 0),      # Centre-align title
   plot.subtitle = element_text(hjust = 0),   # Centre-align subtitle
   legend.title = element_blank(),            # Remove legend title
@@ -37,20 +37,13 @@ theme_update(
 
 # Import data -------------------------------------------------------------
 
-prima_raw <- read_excel('ACROSS For RDT_20240127_MC.xlsx')
-mal_dens_raw <- read_excel('PRIMA-ACROSS_Microcopy and RDT_20230223_MC.xlsx')
+prima_raw <- rio::import('ACROSS For RDT_20240127_MC.xlsx')
 
 # Clean data --------------------------------------------------------------
 
-# Malaria density
-mal_dens <- mal_dens_raw |> 
-  select(CODE, MalDens) |> 
-  janitor::clean_names() |> 
-  arrange(code)
-
 # All data (n = 317)
 prima <- prima_raw |> 
-  select(CODE, AGE, SEX, WEIGHT, TEMP, MRDT, SPC, APC) |>
+  select(CODE, AGE, SEX, WEIGHT, TEMP, MRDT, SPC, PER, EXPER, APC, MD) |>
   janitor::clean_names() |> 
   arrange(code) |> 
   mutate(mrdt = case_when(mrdt == 0 ~ 'Negative',
@@ -74,69 +67,175 @@ prima <- prima_raw |>
                          is.na(spc) ~ NA_character_,
                          TRUE ~ 'Check me!') |> factor(),
          sex = if_else(sex == 1, 'Male', 'Female') |> factor(),
-         apc_per1000 = apc / 1000,
-         log2_apc = if_else(apc == 0, 0, log2(apc)))  # 0 not transformed
+         bcell_counted = case_when(per == 1 ~ 'RBC',
+                                   per %in% c(2, 3, 4) ~ 'WBC',
+                                   is.na(per) ~ NA_character_,
+                                   TRUE ~ 'Check me!') |>
+           factor(levels = c('WBC', 'RBC')),
+         bcell_mult = case_when(
+           bcell_counted == 'RBC' ~ 5e6,
+           bcell_counted == 'WBC' ~ 8000,
+           is.na(bcell_counted) ~ NA_real_,
+           TRUE ~ 9999999),
+         bcell_denom = case_when(
+           bcell_counted == 'RBC' & per == 1 ~ 5000,
+           bcell_counted == 'WBC' & per == 2 ~ 200,
+           bcell_counted == 'WBC' & per == 3 ~ 500,
+           bcell_counted == 'WBC' & per == 4 ~ exper,
+           is.na(bcell_counted) | is.na(per) ~ NA_real_,
+           TRUE ~ 9999999),
+         par_dens = round((apc * bcell_mult) / bcell_denom, digits = 0),
+         log10_par_dens = if_else(par_dens == 0, 0, log10(par_dens))
+         ) 
+
+# Check
+select(.data = prima,
+       apc, per, exper,
+       bcell_counted, bcell_mult, bcell_denom,
+       par_dens, md) |> view()
          
-# Pf-only data (n = 149, n = 148 excluding one patient with missing `apc`)
+# Pf-only data
 prima_pf <- prima |> 
-  drop_na(apc) |> 
   
   # Exclude negative and Pan-only MRDT results
   # i.e., include patients with Pf
   filter(mrdt != 'Negative', mrdt != 'Pan (pLDH)') |> 
   
   # Outcome for regression
-  mutate(double = if_else(mrdt == 'Pf/Pan (HRP-2/pLDH)', 1L, 0L))
+  mutate(double_band = if_else(mrdt == 'Pf/Pan (HRP-2/pLDH)', 1L, 0L) |>
+           factor()) |> 
+  drop_na(par_dens) |> # Exclude missing data on parasite count
+  filter(spc != 'Negative') # Exclude false-positive MRDT
 
 # Descriptives ------------------------------------------------------------
 
 (means <- prima_pf |> 
    group_by(mrdt) |> 
-   summarise(mean_log2 = mean(log2_apc),
-             geo_mean = 2^(mean_log2),
-             median_ori = median(apc),
-             mean_ori = mean(apc)))
+   summarise(mean_log10 = mean(log10_par_dens),
+             geo_mean = 10^(mean_log10),
+             median_ori = median(par_dens),
+             mean_ori = mean(par_dens)))
 
-(apc_histogram_ori <- prima_pf |> 
-  ggplot(aes(x = apc)) +
-  geom_histogram(aes(fill = mrdt), alpha = 0.8, bins = 13) +
-  scale_x_continuous(labels = scales::comma) +
+(dens_histogram_ori <- prima_pf |> 
+  ggplot(aes(x = par_dens / 1000)) +
+  geom_histogram(aes(fill = mrdt),
+                 alpha = 0.8, bins = 14, position = 'identity') +
+  scale_x_continuous(labels = scales::comma,
+                     breaks = seq(0, 1500, by = 500)) +
   scale_y_continuous(expand = c(0, 0, 0, 5),
                      breaks = seq(0, 120, by = 20)) +
-  scale_fill_manual(values = c('gray30', 'gray60')) +
-  theme(legend.position = c(0.8, 0.15),
-        panel.grid.minor = element_blank(),
+  scale_fill_manual(values = c('gray30', 'gray75')) +
+  theme(legend.position = c(0.8, 0.25),
+        panel.grid = element_blank(),
         axis.ticks = element_blank()) +
-  labs(x = '\nParasite density (per \U00B5L)',
+  labs(x = '\nParasite density (in thousand per \U00B5L)',
        y = 'Number of patients\n'))
 
-ggsave(plot = apc_histogram_ori,
-       filename = "apc_histogram_ori.png",
-       height = 3, width = 3,
+ggsave(plot = dens_histogram_ori,
+       filename = "dens_histogram_ori.png",
+       height = 5, width = 5.5,
        dpi = 1200)
 
-(apc_histogram_log <- prima_pf |> 
-    ggplot(aes(x = log2_apc)) +
-    geom_histogram(aes(fill = mrdt), alpha = 0.8, bins = 13) +
-    # scale_x_continuous(labels = scales::comma) +
-    scale_y_continuous(expand = c(0, 0, 0, 5)) +
-    scale_fill_manual(values = c('gray30', 'gray60')) +
-    theme(legend.position = 'right',
-          panel.grid.minor = element_blank(),
+(dens_histogram_ori2 <- prima_pf |> 
+    filter(par_dens < 1e6) |> 
+    ggplot(aes(x = par_dens / 1000)) +
+    geom_histogram(aes(fill = mrdt),
+                   alpha = 0.8, bins = 14, position = 'identity') +
+    scale_x_continuous(labels = scales::comma,
+                       breaks = seq(0, 500, by = 50)) +
+    scale_y_continuous(expand = c(0, 0, 0, 5),
+                       breaks = seq(0, 50, by = 10)) +
+    scale_fill_manual(values = c('gray30', 'gray75')) +
+    theme(legend.position = c(0.8, 0.25),
+          panel.grid = element_blank(),
           axis.ticks = element_blank()) +
-    labs(x = '\nParasite density (per \U00B5L)',
+    labs(x = '\nParasite density (in thousand per \U00B5L)',
          y = 'Number of patients\n'))
 
-ggsave(plot = apc_histogram_ori,
-       filename = "apc_histogram_ori.png",
-       height = 3, width = 5,
+ggsave(plot = dens_histogram_ori2,
+       filename = "dens_histogram_ori2.png",
+       height = 5, width = 5.5,
+       dpi = 1200)
+
+(dens_histogram_log <- prima_pf |> 
+    ggplot(aes(x = log10_par_dens - log10(1000))) +
+    geom_histogram(aes(fill = mrdt),
+                   alpha = 0.8, bins = 14, position = 'identity') +
+    scale_x_continuous(labels = 10^(seq(-1, 3)), breaks = seq(-1, 3)) +
+    scale_y_continuous(expand = c(0, 0, 0, 5),
+                       breaks = seq(0, 50, by = 5)) +
+    scale_fill_manual(values = c('gray30', 'gray75')) +
+    theme(legend.position = c(0.2, 0.8),
+          panel.grid = element_blank(),
+          axis.ticks = element_blank()) +
+    labs(x = '\nParasite density (in thousand per \U00B5L)',
+         y = 'Number of patients\n')
+    # + facet_wrap(~bcell_counted)
+    )
+
+ggsave(plot = dens_histogram_log,
+       filename = "dens_histogram_log.png",
+       height = 5, width = 5.5,
+       dpi = 1200)
+
+(dens_beeswarm <- prima_pf |> 
+  ggplot(aes(x = mrdt, y = log10_par_dens - log10(1000))) +
+  geom_beeswarm(aes(group = mrdt), alpha = 0.2, size = 1.5, cex = 1.8) +
+  geom_boxplot(alpha = 0, width = 0.5/2) +
+  # geom_point(data = means, shape = 18, size = 3, colour = '#D60B00',
+  #            aes(x = mrdt, y = mean_log10 - log10(1000))) +
+  scale_y_continuous(limits = c(-1.5, 3.5),
+                     labels = 10^(seq(-1, 3)), breaks = seq(-1, 3)) +
+  # scale_colour_manual(values = c('gray60', 'gray30')) +
+  theme(legend.position = 'none',
+        panel.grid = element_blank(),
+        axis.ticks = element_blank(),
+        plot.margin = margin(10, 50, 10, 50)) +
+  labs(x = '\nMalaria RDT positive for Plasmodium falciparum',
+       y = 'Parasite density (in thousand per \U00B5L)\n'))
+
+ggsave(plot = dens_beeswarm,
+       filename = "dens_beeswarm.png",
+       height = 4.5, width = 5.5,
        dpi = 1200)
 
 # Modelling ---------------------------------------------------------------
 
-  # negative spc, 0 mal_dens. exclude?
-  # adjust for 4 sites, try also age.
-  # show confidence intervals along the expected probability (ggpredict)
+linear_model <- lm(formula = log10_par_dens ~ double_band + bcell_counted,
+                   data = prima_pf)
+(out_linear <- tidy(linear_model, conf.int = TRUE))
+10^out_linear[2, 2] # Interpretation: higher/lower by a factor of ...
+ggpredict(linear_model, terms = c("double_band",
+                                  "bcell_counted")) |> plot()
+
+logit_model <- glm(formula = double_band ~ log10_par_dens + bcell_counted,
+                   family = "binomial", data = prima_pf)
+(out_logit <- tidy(logit_model, conf.int = TRUE, exponentiate = TRUE))
+
+(marginal_plot <- ggpredict(logit_model,
+                           terms = c("log10_par_dens [0:6, by = 0.01]")) |>
+  plot() +
+  theme_bw() +
+  scale_x_continuous(labels = function(x) comma(10^x),
+                     breaks = seq(0, 6),
+                     expand = c(0, 0)) +
+  scale_y_continuous(expand = c(0.025, 0),
+                     breaks = seq(0, 1, by = 0.2),
+                     labels = label_number(scale = 100, suffix = '%')) +
+  theme(panel.grid.minor = element_blank(),
+        axis.ticks = element_blank(),
+        plot.margin = margin(10, 50, 10, 50),
+        text = element_text(size = 9.5, family = "Fira Code"),
+        axis.title.x = element_text(hjust = 1),   
+        axis.title.y = element_text(hjust = 0.5)) +
+  labs(y = 'Estimated probability\n',
+       x = '\nParasite density (per \U00B5L)',
+       title = ''))
+
+ggsave(plot = marginal_plot,
+       filename = "marginal_plot.png",
+       height = 5.5, width = 7.5,
+       dpi = 1200)
 
 
 
